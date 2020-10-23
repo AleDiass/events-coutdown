dfe7d422fb930a70f10f0779d8e62d622760af30
AleDias <70322758+AleDiass@users.noreply.github.com>
2020-10-23 19:12:44 -0200

    App Js Code Update

--- App.js
+++ App.js
@@ -1,241 +1,153 @@
-const bg = document.getElementById("main-div")
-const title = document.getElementById("event-name")
-const btnNY = document.getElementById("btnNY") 
-const btnES = document.getElementById("btnES")
-const btnCH = document.getElementById("btnCH")
 
 
-//
-const days = document.getElementById('days')
-const hours = document.getElementById('hours')
-const minutes = document.getElementById('minutes')
-const seconds = document.getElementById('seconds')
+	const days = document.getElementById('days')
+	const hours = document.getElementById('hours')
+	const minutes = document.getElementById('minutes')
+	const seconds = document.getElementById('seconds')
+	
+	// Start Timer Var
+	let Start;
 
 
+let ClickButton = function(Target){
+	let AtualTimeBTN = new Date()
 
+	// Change BackGround
 
-//
-let StartNY;
-let StartES;
-let StartCH;
-
-
-
-function App(event){
+	ChangeBG(Target)
+	DisableButtons(Target)
 	
 
-	// Change Bg Image
+	// Check Year // 
 
-	bg.style.background = `url(./imgs/${event}.jpg) center`
-	bg.style.backgroundSize = 'cover'
+	let ChristmasTime = new Date(AtualTimeBTN.getFullYear(),11,24)
 	
 	
-
 	
-
-	// Change Timer //  Title
-	if(event ==='NY'){
-		title.innerText = 'New Year Countdown'
-
-		StartNY = setInterval(Newyear,1000)		
-
-		clearInterval(StartES)
-		clearInterval(StartCH)
-
-		// Disable Button
-		ButtonStyleAdd(btnNY)
-
-		//Enable Buttons
-		ButtonStyleRemove(btnES,btnCH)
-
-
-
-	}
-	else if(event ==='ES'){
-		title.innerText = 'Easter Countdown'
-			
-		
-		StartES = setInterval(Easter,1000)	
-		clearInterval(StartNY)
-		clearInterval(StartCH)
-
-		//Disable Button
-		ButtonStyleAdd(btnES)
-		
-		//Enable Buttons
-		ButtonStyleRemove(btnNY,btnCH)
-
-	}
-	else if(event === 'CH'){
-		title.innerText = 'Christmas Countdown'
-
-		
-		
-		StartCH = setInterval(Chrism,1000)	
+	//
+	let EasterTime = new Date(AtualTimeBTN.getFullYear(),03,03)
+	//
 	
-		clearInterval(StartNY)
-		clearInterval(StartES)
-		
-		// Disable Button		
-		ButtonStyleAdd(btnCH)
-
-		
-		//Enable Buttons
-		ButtonStyleRemove(btnNY,btnES)
-
-
-	}
-
-
-}
-
-
-
-
-
-function ButtonStyleAdd(button){
-	button.disabled = true
-	button.style.opacity = '0.4'
-	button.style.cursor = 'default'
-	button.style.border = '1px white solid'
-}
-
-function ButtonStyleRemove(button1,button2){
-	button1.disabled = false
-	button1.style.opacity = '1'
-	button1.style.cursor = 'pointer'
-	button1.style.border = '1px black solid'
-
-	button2.disabled = false
-	button2.style.opacity = '1'
-	button2.style.cursor = 'pointer'
-	button2.style.border = '1px black solid'
-}
-
-
-
-
-
-
-
-
-
-function Newyear(){
-
-	//Convert Time Countdown
-	let	AtualN = new Date()
-	
-	let NY_Base = new Date(AtualN.getFullYear()+1,00,01)
-	
-	let NY_Diff = 		(AtualN.getTime()- NY_Base.getTime()) / 1000
-	
-	let NY_daysDiff =		   NY_Diff / (3600*24)
-	
-	let NY_HoursDiff = 		  (NY_daysDiff*24)%24
-
-	let NY_minDiff =	 	  (NY_HoursDiff*60)%60
-	
-	let NY_secDiff =		  (NY_minDiff*60)%60
-	
-	//Insert Values
-	days.innerText = Math.abs(Math.ceil(NY_daysDiff))
-	hours.innerText = Math.abs(Math.ceil(NY_HoursDiff))
-	minutes.innerText = Math.abs(Math.ceil(NY_minDiff))
-	seconds.innerText = Math.abs(Math.ceil(NY_secDiff))
-	
-	CheckValue()
-	
-}
-
-
-function Easter(){
-	
-	let AtualE = new Date()
-
-
-	let ES_Base = new Date(AtualE.getFullYear(),03,04)
-
-
-
-	if(AtualE.getTime() >ES_Base.getTime() ){
-		ES_Base.setFullYear(AtualE.getFullYear()+1)
+	// Check Event Date
+	if(AtualTimeBTN.getTime() > EasterTime.getTime()){
+		EasterTime.setFullYear(AtualTimeBTN.getFullYear()+1)
 	}
 
 	
-	//Convert Time Countdown	
 
-	let ES_Diff = (AtualE.getTime() - ES_Base.getTime()) / 1000
-	
-	let ES_daysDiff = ES_Diff / (3600*24)
-	
-	let ES_HoursDiff = (ES_daysDiff*24)%24
-	
-	let ES_minDiff =  (ES_HoursDiff*60)%60
-	
-	let ES_secDiff = (ES_minDiff*60)%60
-	
+	//
+	let NewYearTime = new Date(AtualTimeBTN.getFullYear()+1,00,01)
 
-
-	//Insert Values
-	days.innerText = Math.abs(Math.ceil(ES_daysDiff))
-	hours.innerText = Math.abs(Math.ceil(ES_HoursDiff))
-	minutes.innerText = Math.abs(Math.ceil(ES_minDiff))
-	seconds.innerText = Math.abs(Math.ceil(ES_secDiff))
-
-	
-	
-
-	
-	CheckValue()
-
-}
-
-
-
-
-function Chrism(){
-	let AtualC = new Date()
-
-	let CH_Base = new Date(AtualC.getFullYear(),11,25)
-	
-	if(AtualC.getTime() > CH_Base.getTime()){
+	//
+	if(AtualTimeBTN.getTime() > NewYearTime.getTime()){
 		
-		CH_Base.setFullYear(CH_Base.getFullYear()+1)
-	
+		NewYearTime.setFullYear(AtualTimeBTN.getFullYear()+1)
 	}
 
-
-
-	//Convert Time Countdown
-	let CH_Diff = (AtualC.getTime() - CH_Base.getTime()) / 1000
-	let CH_daysDiff = CH_Diff / (3600*24)
-	let CH_HoursDiff = (CH_daysDiff*24)%24
-	let CH_minDiff = (CH_HoursDiff*60)%60
-	let CH_secDiff = (CH_minDiff*60)%60
-
-	//Insert Values
-	days.innerText = Math.abs(Math.ceil(CH_daysDiff))
-	hours.innerText = Math.abs(Math.ceil(CH_HoursDiff))
-	minutes.innerText = Math.abs(Math.ceil(CH_minDiff))
-	seconds.innerText = Math.abs(Math.ceil(CH_secDiff))
-	
-	CheckValue()
 	
 
 	
+	// Start
+	
+	if(Target == 'CH'){
+		clearInterval(Start)
+		Start = setInterval(()=>Timer(ChristmasTime),1000)
+	}
+
+	if(Target == 'NY'){
+		clearInterval(Start)
+		Start = setInterval(()=>Timer(NewYearTime),1000)
+	}
+
+	if(Target == 'ES'){
+		clearInterval(Start)
+		Start = setInterval(()=>Timer(EasterTime),1000)
+	}
+	
+}	
 
 
-}				
 
 
 
 
-function CheckValue(){
-	days.innerText < 10? days.innerText = '0'+ days.innerText: days.innerText
-	hours.innerText < 10? hours.innerText = '0'+ hours.innerText: hours.innerText
-	minutes.innerText < 10? minutes.innerText = '0'+ minutes.innerText: minutes.innerText
-	seconds.innerText < 10? seconds.innerText = '0'+ seconds.innerText: seconds.innerText
+function Timer(event){
+	
 
+	let AtualTime = new Date()
+	
+	AtualTime = AtualTime.getTime()
+
+	let ReferenceTime = event.getTime()
+
+	
+	let TimeDiff =  Math.abs(AtualTime - ReferenceTime)
+
+	let DaysTime = TimeDiff / (3600*24*1000) 
+	let HoursTime = (DaysTime * 24)% 24
+	let MinutesTime = (HoursTime * 60) % 60 
+	let SecTime = (MinutesTime * 60) % 60
+	
+	
+
+
+	// Format Values
+
+	DaysTime = Math.trunc(DaysTime)
+	HoursTime = Math.trunc(HoursTime)
+	MinutesTime = Math.trunc(MinutesTime)
+	SecTime = Math.trunc(SecTime)
+
+	// Check Values
+
+
+	DaysTime < 10 ? DaysTime = '0' + DaysTime : DaysTime
+	//
+	HoursTime < 10 ? HoursTime = '0' + HoursTime : HoursTime
+	//
+	MinutesTime < 10 ? MinutesTime = '0' + MinutesTime : MinutesTime
+	//
+	SecTime < 10 ? SecTime = '0' + SecTime : SecTime
+
+
+	// Insert Date in Front App
+	days.innerText = DaysTime
+	hours.innerText = HoursTime
+	minutes.innerText = MinutesTime
+	seconds.innerText = SecTime
+	
+	
 
 }
 
+
+
+function ChangeBG(Event){
+	// Change BackGround
+	document.getElementById('main-div').style.background = `url(./imgs/${Event}.jpg) Center`
+	document.getElementById('main-div').style.backgroundSize = 'cover'
+}
+
+
+
+function DisableButtons(btn){
+		
+		document.querySelectorAll('#buttons input').forEach((btns)=>
+		{
+			// Enable Buttons
+			btns.disabled = false
+			btns.style.opacity = '1'
+			btns.style.cursor = 'pointer'
+
+
+		})
+
+
+		// Disable Clicked Button
+		btn = document.getElementById(btn)
+		btn.disabled = true
+		btn.style.opacity = '0.4'	
+		btn.style.cursor = 'default'
+	
+
+}
--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # events-coutdown
-
-
-
-**A Simple New Year / Easter / Christmas Countdown** 
-
-Demo : **https://alediass.github.io/events-coutdown/**
-
-**JavaScript , css , html**
-
+A Simple New Year / Easter / Christmas Countdown 
+Demo: https://alediass.github.io/events-coutdown/
